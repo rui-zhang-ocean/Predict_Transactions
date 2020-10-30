85d942eedd18c4646eae84b218d00d1f825ff200
rui-zhang-ocean <rzhang.dal@gmail.com>
2020-10-30 12:13:21 -0400

    check synthetic data trained from GCP

---
+++ resave_pickle.py
@@ -0,0 +1,17 @@
+import pickle
+
+# load synthesizer from saved object
+with open('/home/rzhang_dal/project/TVAE_synthesizer.pkl', 'rb') as input:
+    synthesizer = pickle.load(input)
+
+# check out sample
+sampled = synthesizer.sample(3)
+np.set_printoptions(suppress = True, precision = 2)
+print(sampled)
+
+# resave the synthesizer
+def save_object(obj, filename):
+    with open(filename, 'wb') as output:  # overwrite any existing file
+        pickle.dump(obj, output, pickle.DEFAULT_PROTOCOL)
+
+save_object(synthesizer, '/home/rzhang_dal/project/TVAE_synthesizer_1.pkl')
